09556f43f0290cb234435fdb0cc3607cc22569a1
martinikf <martinik.filip01@gmail.com>
2022-11-11 16:23:35 +0100

    Seed create methods

---
+++ TOS/Data/Seed.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TOS.Models;
+
+namespace TOS.Data;
+
+public static class Seed
+{
+    public static void InitSeed()
+    {
+        //Create Roles: Administrator, Teacher, Student, External
+        
+        //Create administrator user
+        
+        //Add roles to administrator
+        
+        //Create default Groups: Unassigned, Bachelor, Master for topics
+        
+        
+        
+        
+    }
+
+
+
+    public static ApplicationUser CreateUser(string firstname, string lastname, string email, bool emailConfirmed, string password, ApplicationDbContext ctx)
+    {
+        //Create usef if not exists
+        var user = ctx.Users.FirstOrDefault(u => u.Email == email);
+        if (user == null)
+        {
+            user = new ApplicationUser
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                Email = email,
+                EmailConfirmed = emailConfirmed,
+                UserName = email
+            };
+            user.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(user, password);
+            user.SecurityStamp = Guid.NewGuid().ToString("D");
+            
+            ctx.Users.Add(user);
+            ctx.SaveChanges();
+        }
+        
+        return user;
+    }
+
+    public static IdentityRole<int> CreateRole(string name, ApplicationDbContext ctx)
+    {
+        //Create new IdentityRole if not exists
+        var role = ctx.Roles.FirstOrDefault(r => r.Name.Equals(name));
+        if (role == null)
+        {
+            role = new IdentityRole<int>
+            {
+                Name = name,
+                NormalizedName = name.ToUpper()
+            };
+            ctx.Roles.Add(role);
+            ctx.SaveChanges();
+        }
+        
+        return role;
+    }
+    
+    public static IdentityUserRole<int> CreateUserRole(ApplicationUser user, IdentityRole<int> role, ApplicationDbContext ctx)
+    {
+       //Create new IdentityUserRole if not exists
+            var userRole = ctx.UserRoles.FirstOrDefault(r => r.UserId.Equals(user.Id) && r.RoleId.Equals(role.Id));
+            if (userRole == null)
+            {
+                userRole = new IdentityUserRole<int>
+                {
+                    UserId = user.Id,
+                    RoleId = role.Id
+                };
+                ctx.UserRoles.Add(userRole);
+                ctx.SaveChanges();
+            }
+            
+            return userRole;
+    }
+
+    public static Attachment CreateAttachment(string name, string path, DateTime createdAt, ApplicationUser creator, Topic topic, ApplicationDbContext ctx)
+    {
+        //Create new Attachment if not exists
+        var attachment = ctx.Attachments.FirstOrDefault(a =>
+            a.Name.Equals(name) && a.Path.Equals(path) && a.Creator.Id.Equals(creator.Id) && a.Topic.TopicId.Equals(topic.TopicId));
+        
+        if (attachment == null)
+        {
+            attachment = new Attachment
+            {
+                Name = name,
+                Path = path,
+                CreatedAt = createdAt,
+                Creator = creator,
+                Topic = topic
+            };
+            ctx.Attachments.Add(attachment);
+            ctx.SaveChanges();
+        }
+        
+        return attachment;
+    }
+
+    public static Comment CreateComment(string text, DateTime createAt, ApplicationUser author, Comment? parentComment, Topic topic, bool anonymous, ApplicationDbContext ctx)
+    {
+        //Create new Comment if not exists
+        var comment = ctx.Comments.FirstOrDefault(c =>
+            c.Text.Equals(text) && c.Author.Id.Equals(author.Id) && 
+            (c.ParentComment == null && parentComment == null || (c.ParentComment != null && parentComment != null && c.ParentComment.CommentId.Equals(parentComment.CommentId)))
+            && c.Topic.TopicId.Equals(topic.TopicId) && c.Anonymous.Equals(anonymous));
+
+        if (comment == null)
+        {
+            comment = new Comment()
+            {
+                Text = text,
+                CreatedAt = createAt,
+                Author = author,
+                ParentComment = parentComment,
+                Topic = topic,
+                Anonymous = anonymous
+            };
+            ctx.Comments.Add(comment);
+            ctx.SaveChanges();
+        }
+        
+        return comment;
+    }
+
+    public static Group CreateGroup(string name, ApplicationUser creator, bool selectable, bool visible, ApplicationDbContext ctx)
+    {
+        //Create group if not exists
+        var group = ctx.Groups.FirstOrDefault(g => g.Name.Equals(name) /*&& g.Creator.Id.Equals(creator.Id) && g.Selectable.Equals(selectable) && g.Visible.Equals(visible)*/);
+        if (group == null)
+        {
+            group = new Group()
+            {
+                Name = name,
+                Creator = creator,
+                Selectable = selectable,
+                Visible = visible
+            };
+            ctx.Groups.Add(group);
+            ctx.SaveChanges();
+        }
+
+        return group;
+    }
+
+    public static Programme CreateProgramme(string name, bool active, ProgramType type, ApplicationDbContext ctx)
+    {
+        //Create programme if not exists
+        var programme = ctx.Programmes.FirstOrDefault(p => p.Name.Equals(name) && p.Type.Equals(type));
+        if (programme == null)
+        {
+            programme = new Programme()
+            {
+                Name = name,
+                Active = active,
+                Type = type
+            };
+            ctx.Programmes.Add(programme);
+            ctx.SaveChanges();
+        }
+        
+        return programme;
+    }
+
+    public static Topic CreateTopic(string name, string description, bool visible, ApplicationUser creator,
+        ApplicationUser? supervisor, ApplicationUser? assignedStudent, Group? group, ApplicationDbContext ctx)
+    {
+        //Crate new topic if not exists
+        var topic = ctx.Topics.FirstOrDefault(t => t.Name.Equals(name));
+
+        if (topic == null)
+        {
+            topic = new Topic()
+            {
+                Name = name,
+                Description = description,
+                Visible = visible,
+                Creator = creator,
+                Supervisor = supervisor,
+                AssignedStudent = assignedStudent,
+                Group = group
+            };
+            ctx.Topics.Add(topic);
+            ctx.SaveChanges();
+        }
+        
+        return topic;
+    }
+
+    public static TopicRecommendedProgramme CreateTopicRecommendedProgramme(Programme program, Topic topic, ApplicationDbContext ctx)
+    {
+        //Create topic recommended programme if not exists
+        var topicRecommendedProgramme = ctx.TopicRecommendedProgrammes.FirstOrDefault(t =>
+            t.Programme.ProgrammeId.Equals(program.ProgrammeId) && t.Topic.TopicId.Equals(topic.TopicId));
+
+        if (topicRecommendedProgramme == null)
+        {
+            topicRecommendedProgramme = new TopicRecommendedProgramme()
+            {
+                Programme = program,
+                Topic = topic
+            };
+            ctx.TopicRecommendedProgrammes.Add(topicRecommendedProgramme);
+            ctx.SaveChanges();
+        }
+        
+        return topicRecommendedProgramme;
+    }
+    
+    public static UserInterestedTopic CreateUserInterestedTopic(ApplicationUser user, Topic topic, ApplicationDbContext ctx)
+    {
+        //Create user interested topic if not exists
+        var userInterestedTopic = ctx.UserInterestedTopics.FirstOrDefault(u =>
+            u.User.Id.Equals(user.Id) && u.Topic.TopicId.Equals(topic.TopicId));
+
+        if (userInterestedTopic == null)
+        {
+            userInterestedTopic = new UserInterestedTopic()
+            {
+                User = user,
+                Topic = topic
+            };
+            ctx.UserInterestedTopics.Add(userInterestedTopic);
+            ctx.SaveChanges();
+        }
+        
+        return userInterestedTopic;
+    }
+    
+}
