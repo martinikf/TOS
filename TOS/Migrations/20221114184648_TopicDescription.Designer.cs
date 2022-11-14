317a42621f5754dcc4d448c43c5d6dfaf1721a2d
Filip Martiník <martinik.filip01@gmail.com>
2022-11-14 19:50:07 +0100

    Topic.Descriptions

--- TOS/Migrations/20221114184648_TopicDescription.Designer.cs
+++ TOS/Migrations/20221114184648_TopicDescription.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TOS.Data;
@@ -11,9 +12,10 @@ using TOS.Data;
 namespace TOS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221114184648_TopicDescription")]
+    partial class TopicDescription
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TOS/Migrations/20221114184648_TopicDescription.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TOS.Migrations
+{
+    public partial class TopicDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Topic",
+                newName: "DescriptionLong");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateTime",
+                table: "UserInterestedTopic",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionShort",
+                table: "Topic",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateTime",
+                table: "UserInterestedTopic");
+
+            migrationBuilder.DropColumn(
+                name: "DescriptionShort",
+                table: "Topic");
+
+            migrationBuilder.RenameColumn(
+                name: "DescriptionLong",
+                table: "Topic",
+                newName: "Description");
+        }
+    }
+}
