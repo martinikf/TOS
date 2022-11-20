75b053193e53a7f89d3a429cc1571c4899bca94f
martinikf <martinik.filip01@gmail.com>
2022-11-20 12:20:33 +0100

    EmailSender

--- TOS/Program.cs
+++ TOS/Program.cs
@@ -1,4 +1,3 @@
-using System.Configuration;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -25,18 +24,21 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.R
 builder.Services.AddControllersWithViews();
 
 //Register email sender
-builder.Services.AddTransient<SendinBlueSettings, SendinBlueSettings>(
-    a => new SendinBlueSettings()
+builder.Services.AddSingleton<SmtpEmailSenderSettings, SmtpEmailSenderSettings>(
+    _ => new SmtpEmailSenderSettings()
     {
-        Username = builder.Configuration["SendinBlue:Username"],
-        Password = builder.Configuration["SendinBlue:Password"]
+        Username = builder.Configuration["SendinBlue:Username"] ?? throw new Exception("Secrets are not set"),
+        Password = builder.Configuration["SendinBlue:Password"] ?? throw new Exception("Secrets are not set"),
+        Port = 587,
+        FromAddress = "noreply@tos.tos",
+        SmtpServer = "smtp-relay.sendinblue.com",
+        EnableSsl = true
     });
-builder.Services.AddTransient<IEmailSender, EmailSender>();
-
+builder.Services.AddSingleton<IEmailSender, SmtpEmailSender>();
 
 var app = builder.Build();
 
-if(false)
+if(true)
     Seed.InitSeed(app);
 
 // Configure the HTTP request pipeline.
---
+++ TOS/Services/SmtpEmailSender.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace TOS.Services;
+
+public class SmtpEmailSender : IEmailSender
+{
+    private readonly SmtpClient _client;
+    private readonly string _fromAddress;
+
+    public SmtpEmailSender(SmtpEmailSenderSettings settings)
+    {
+        _fromAddress = settings.FromAddress;
+        var credentials = new NetworkCredential(settings.Username, settings.Password);
+        
+        _client = new SmtpClient(settings.SmtpServer)
+        {
+            Port = settings.Port,
+            Credentials = credentials,
+            EnableSsl = settings.EnableSsl
+        };
+    }
+    
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        return _client.SendMailAsync(new MailMessage(_fromAddress, email, subject, htmlMessage));
+    }
+}
+
+public class SmtpEmailSenderSettings
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string FromAddress { get; set; } = string.Empty;
+    public string SmtpServer { get; set; } = string.Empty;
+    public int Port { get; set; }
+    public bool EnableSsl { get; set; }
+}
