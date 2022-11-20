a403b023542ec9b27f4825c9eb061cb967ea2ce6
martinikf <martinik.filip01@gmail.com>
2022-11-20 20:00:50 +0100

    Login rework

--- TOS/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TOS/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -7,14 +7,12 @@ using System.DirectoryServices.Protocols;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Text.Encodings.Web;
 using System.Xml.Linq;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using TOS.Data;
 using TOS.Models;
 
@@ -27,6 +25,7 @@ namespace TOS.Areas.Identity.Pages.Account
         private readonly ILogger<LoginModel> _logger;
         private readonly ApplicationDbContext _context;
         private readonly IEmailSender _emailSender;
+        
         public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger, ApplicationDbContext context, IEmailSender emailSender, UserManager<ApplicationUser> userManager)
         {
             _signInManager = signInManager;
@@ -108,138 +107,70 @@ namespace TOS.Areas.Identity.Pages.Account
             ReturnUrl = returnUrl;
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        private void PrepareInputUsername()
         {
-            returnUrl ??= Url.Content("~/");
-
-            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-
-            //Else external login
-            if (ModelState.IsValid)
+            if (Input.Username.EndsWith("@upol.cz"))
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                
-                if (result.Succeeded)
+                //User used his email
+                var userFromEmailInput = _context.Users.FirstOrDefault(x => x.Email.Equals(Input.Username));
+                if (userFromEmailInput != null)
                 {
-                    //Used also for AD users with local password
-                    _logger.LogInformation("User logged in");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out");
-                    return RedirectToPage("./Lockout");
+                    if(userFromEmailInput.UserName is null) throw new Exception("User has no username");
+                    Input.Username = userFromEmailInput.UserName;
                 }
                 else
                 {
-                    //For AD users
-
-                    //Login for AD users with account in database
-                    //Check if username exists in database
-                    var user = _context.Users.FirstOrDefault(u => u.UserName == Input.Username);
-
-                    if (user is null)
-                    {
-                        //AD user in database used an email instead of portal login username
-                        //Happens if User is already in database and entered full email instead of username
-                        var adUserByEmail =
-                            _context.Users.FirstOrDefault(u => u.Email.ToLower().Equals(Input.Username.ToLower()));
-                        if (adUserByEmail != null)
-                        {
-                            if (adUserByEmail.UserName == null) throw new Exception("User doesn't have username");
-                            
-                            //Local password check
-                            result = await _signInManager.PasswordSignInAsync(adUserByEmail.UserName, Input.Password,
-                                Input.RememberMe, lockoutOnFailure: false);
-                            if (result.Succeeded)
-                            {
-                                return LocalRedirect(returnUrl);
-                            }
-
-                            //Check for AD auth
-                            if (LdapAuthenticate(adUserByEmail.UserName, Input.Password))
-                            {
-                                //Sign in user
-                                await _signInManager.SignInAsync(adUserByEmail, Input.RememberMe);
-                                return LocalRedirect(returnUrl);
-                            }
-                            
-                            //Failed login
-                            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                            return Page();
-                        }
-                        
-                        //Happens if user used username + @upol.cz
-                        if ( Input.Username.EndsWith("@upol.cz"))
-                        {
-                            //Trim the @upol.cz part
-                            Input.Username = Input.Username.Replace("@upol.cz", "");
-                            
-                            //set the user with trimmed username
-                            user = _context.Users.FirstOrDefault(u => u.UserName.Equals(Input.Username));
-                            
-                            //Have to check again for local password login
-                            //Check if user tried to logged in with local password
-                            result = await _signInManager.PasswordSignInAsync(Input.Username, Input.Password,
-                                Input.RememberMe, lockoutOnFailure: false);
-                            if (result.Succeeded)
-                            {
-                                return LocalRedirect(returnUrl);
-                            }
-                            //Check for AD login later.
-
-                        }
-                    }
-
-                    if (user != null)
-                    {
-                        if (LdapAuthenticate(Input.Username, Input.Password))
-                        {
-                            //Sign in user
-                            await _signInManager.SignInAsync(user, Input.RememberMe);
-                            return LocalRedirect(returnUrl);
-                        }
-                    }
-                    
-                    //If user is not in database, check if AD credentials are valid -> CreateUser
-                    ApplicationUser createdUser;
-                    if ((createdUser = CreateLdapUser()) != null)
-                    {
-                        //Sign in new user
-                        await _signInManager.SignInAsync(createdUser, Input.RememberMe);
-
-                        //Generate email confirmation
-                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(createdUser);
-                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                        var callbackUrl = Url.Page(
-                            "/Account/ConfirmEmail",
-                            pageHandler: null,
-                            values: new { area = "Identity", userId = createdUser.Id, code, returnUrl = "/"},
-                            protocol: Request.Scheme);
-
-                        if(callbackUrl is null) throw new Exception("CallbackUrl is null");
-
-                        if (createdUser.Email is null) throw new Exception("Email is null, STAGAPI fetch failed");
-                        
-                        //Send email confirmation
-                        await _emailSender.SendEmailAsync(createdUser.Email, "Confirm your email",
-                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-                        
-                        _logger.LogInformation("User logged in");
-                        return LocalRedirect(returnUrl);
-                    }
-                    
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
+                    //User used portalID + @upol.cz
+                    Input.Username = Input.Username.Replace("@upol.cz", "");
                 }
             }
 
-            // If we got this far, something failed, redisplay form
-            return Page();
+            Input.Username = Input.Username.ToLower();
         }
-        
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            
+            //Convert input username to lowercase
+            Input.Username = Input.Username.ToLower();
+
+            //Set Input.Username to correct format
+            PrepareInputUsername();
+
+            var successfulSignIn = false; //result shows if user was authenticated
+            var user = _context.Users.FirstOrDefault(x => x.UserName.Equals(Input.Username));
+
+            if (user != null)
+            {
+                //User is in database ------
+                
+                //Authenticate user with server database
+                if (user.PasswordHash != null)
+                {
+                    successfulSignIn = await DatabaseSignIn(user, false);
+                }
+                
+                //Authenticate user with LDAP UPOL domain
+                if (!successfulSignIn)
+                {
+                    successfulSignIn = await LdapSignIn(user);
+                }
+            }
+            else
+            {
+                //User not found in database -> register -> sign in 
+                ApplicationUser createdUser;
+                if ((createdUser = CreateLdapUser()) != null)
+                {
+                    successfulSignIn = await LdapSignIn(createdUser);
+                }
+            }
+
+            //Successful login
+            return successfulSignIn ? LocalRedirect(returnUrl) : InvalidLoginAttempt();
+        }
+
         private bool LdapAuthenticate(string username, string password)
         {
             try
@@ -274,33 +205,21 @@ namespace TOS.Areas.Identity.Pages.Account
         {
             try
             {
-                //If AD authentication fails -> return false
                 if (!LdapAuthenticate(Input.Username, Input.Password)) return null;
 
-                var student = true;
+                //Decide if user is a teacher or student and get their userStagId
+                string userStagId;
+                const string stagServicesUrl = "https://stagservices.upol.cz/ws/services";
                 
-                //Decide if user is a teacher or student and get OsCislo
-                string osCislo;
-                var studentReq =
-                    "https://stagservices.upol.cz/ws/services/rest2/users/getOsobniCislaByExternalLogin?login=" + Input.Username;
+                var studentReq = stagServicesUrl + "/rest2/users/getOsobniCislaByExternalLogin?login=" + Input.Username;
+                if ((userStagId = GetUserStagId(studentReq)).Length >= 1) return CreateUser(userStagId, true);
                 
-                if ((osCislo = GetOsCislo(studentReq)).Length < 1)
-                {
-                    //Student osCislo not found -> try teacher
-                    student = false;
-                    string teacherReq =
-                        "https://stagservices.upol.cz/ws/services/rest2/users/getUcitIdnoByExternalLogin?login=" +
-                        Input.Username;
-
-                    if ((osCislo = GetOsCislo(teacherReq)).Length < 1)
-                    {
-                        //Teacher osCislo not found -> return null
-                        return null;
-                    }
-                }
-
-                //Create user to database
-                return CreateUser(osCislo, student);
+                //Student ID not found -> try teacher
+                var teacherReq = stagServicesUrl + "/rest2/users/getUcitIdnoByExternalLogin?login=" + Input.Username;
+                if ((userStagId = GetUserStagId(teacherReq)).Length >= 1)  return CreateUser(userStagId, false);
+                
+                //Couldn't find any userStagId
+                return null;
             }
             catch
             {
@@ -315,12 +234,10 @@ namespace TOS.Areas.Identity.Pages.Account
                 ? "https://stagservices.upol.cz/ws/services/rest2/student/getStudentInfo?osCislo=" + osCislo
                 : "https://stagservices.upol.cz/ws/services/rest2/ucitel/getUcitelInfo?ucitIdno=" + osCislo;
 
-            var info = GetInfo(req);
-            
-            //If info values are null, stagapi connection failed
+            var info = GetUserStagInfo(req);
 
             var lastnameLowered = info.Item2[..1] + info.Item2[1..].ToLower();
-            var user = Seed.CreateUser(info.Item1, lastnameLowered, info.Item3, Input.Username,false, null, _context);
+            var user = Seed.CreateUser(info.Item1, lastnameLowered, info.Item3, Input.Username,true, null, _context);
             
             var roleToInsert = _context.Roles.FirstOrDefault(x => x.Name.Equals(roleString));
             if (roleToInsert is null) throw new Exception("Role that should exist does not exist: " + roleString);
@@ -330,7 +247,7 @@ namespace TOS.Areas.Identity.Pages.Account
             return user;
         }
 
-        private Tuple<string, string, string> GetInfo(string reqString)
+        private Tuple<string, string, string> GetUserStagInfo(string reqString)
         {
             try
             {
@@ -354,15 +271,11 @@ namespace TOS.Areas.Identity.Pages.Account
             }
             catch
             {
-                _logger.LogInformation("Stag api connection failed");
-                ModelState.AddModelError(string.Empty, "Connection to STAGAPI failed.");
-                //return to login page
-                RedirectToAction("OnGetAsync");
                 throw new Exception("STAGAPI connection failed");
             }
         }
 
-        private string GetOsCislo(string reqString)
+        private string GetUserStagId(string reqString)
         {
             try
             {
@@ -378,13 +291,34 @@ namespace TOS.Areas.Identity.Pages.Account
             }
             catch
             {
-                //Stag api connection failed
-                _logger.LogInformation("Stag api connection failed");
-                ModelState.AddModelError(string.Empty, "Connection to STAGAPI failed.");
-                //return to login page
-                RedirectToAction("OnGetAsync");
                 throw new Exception("STAGAPI connection failed");
             }
+        }
+        
+        private async Task<bool> DatabaseSignIn(ApplicationUser user, bool lockoutOnFailure)
+        {
+            if (user.UserName is null)
+                throw new Exception("User does not have a username");
+
+            var result =
+                await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe,
+                    lockoutOnFailure);
+            
+            return result.Succeeded;
+        }
+
+        private async Task<bool> LdapSignIn(ApplicationUser user)
+        {
+            if (!LdapAuthenticate(user.UserName, Input.Password)) return false;
+            
+            await _signInManager.SignInAsync(user, Input.RememberMe);
+            return true;
+        }
+
+        private IActionResult InvalidLoginAttempt()
+        {
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return Page();
         }
     }
 }
