9866f57620fd03be7b78dbd9e609ef0e365de056
martinikf <martinik.filip01@gmail.com>
2022-12-03 23:15:33 +0100

    Interest button in topic Details.cshtml

--- TOS/Controllers/TopicController.cs
+++ TOS/Controllers/TopicController.cs
@@ -28,14 +28,16 @@ namespace TOS.Controllers
         {
 
             Group? group = null;
+            //Used for showing edit button for not selectable groups (teacher created groups)
             ViewData["TopicsIndexGroupId"] = null;
             
-            if (groupName != null) group = _context.Groups.FirstOrDefault(x => x.Name.Equals(groupName));
+            if (groupName != null) group = await _context.Groups.FirstOrDefaultAsync(x => x.Name.Equals(groupName));
             
             if (group == null)
             {
                 ViewData["TopicsIndexHeading"] = _sharedLocalizer["Topics"];
-
+                ViewData["TopicsIndexGroupName"] = null;
+                
                 //If groupId wasn't provided or groupId is invalid -> Display all Bachelor and Master topics
                 var applicationDbContext = _context.Topics.Where(x => x.Group.Selectable && x.Group.Visible)
                     .Include(t => t.AssignedStudent).Include(t => t.Creator)
@@ -46,11 +48,14 @@ namespace TOS.Controllers
             {
                 ViewData["TopicsIndexHeading"] = _sharedLocalizer[groupName];
                 
+                //Shows edit button only for not selectable groups
                 if (!group.Selectable)
                 {
                     ViewData["TopicsIndexGroupId"] = group.GroupId;
                 }
 
+                ViewData["TopicsIndexGroupName"] = group.Name;
+                
                 var applicationDbContext = _context.Topics.Where(x => x.Group.Name.Equals(groupName) && x.Visible)
                     .Include(t => t.AssignedStudent).Include(t => t.Creator)
                     .Include(t => t.Group).Include(t => t.Supervisor);
@@ -76,17 +81,66 @@ namespace TOS.Controllers
             {
                 return NotFound();
             }
+            
+            //Get cuurent user
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName.Equals(User.Identity.Name));
+
+            if (await _context.UserInterestedTopics.AnyAsync(x => x.UserId.Equals(user.Id) && x.TopicId.Equals(id)))
+            {
+                @ViewData["UserInterestClass"] = "interested";
+                @ViewData["UserInterestString"] = _sharedLocalizer["Remove interest"];
+            }
+            else
+            {
+
+                @ViewData["UserInterestClass"] = "not-interested";
+                @ViewData["UserInterestString"] = _sharedLocalizer["Add interest"];
+            }
 
             return View(topic);
         }
 
         // GET: Topic/Create
-        public IActionResult Create()
+        public IActionResult Create(string? groupName = null)
         {
-            ViewData["AssignedId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["GroupId"] = new SelectList(_context.Groups.Where(x=>x.Selectable), "GroupId", "GroupId");
-            ViewData["SupervisorId"] = new SelectList(_context.Users, "Id", "Id");
+            
+            SelectList? groupSelectList = null;
+            
+            if (groupName == null)
+            {
+                groupSelectList = new SelectList(_context.Groups.Where(x => x.Selectable), "GroupId", "Name");
+            }
+            else
+            {
+                //Creates selectList for groups when a valid groupName is provided
+                var groupProvided = _context.Groups.First(x => x.Name.ToLower().Equals(groupName.ToLower()));
+                if (!groupProvided.Selectable)
+                {
+                    groupSelectList = new SelectList(_context.Groups.Where(x => x.Selectable || x.Equals(groupProvided)),
+                        "GroupId", "Name", groupProvided.GroupId);
+                }
+                else
+                {
+                    groupSelectList = new SelectList(_context.Groups.Where(x => x.Selectable), 
+                        "GroupId", "Name", groupProvided.GroupId);
+                }
+            }
+
+            ViewData["Groups"] = groupSelectList;
+            var studentRole = _context.Roles.First(x => x.Name != null && x.Name.Equals("Student"));
+            ViewData["Students"] =
+                new SelectList(
+                    _context.Users.Where(user =>
+                        _context.UserRoles.Any(ur => ur.UserId == user.Id && ur.RoleId == studentRole.Id)), "Id",
+                    "Email");
+            
+            //Selects all users with role Teacher
+            var teacherRole = _context.Roles.First(r => r.Name != null && r.Name.Equals("Teacher"));
+            ViewData["Supervisors"] =
+                new SelectList(
+                    _context.Users.Where(x =>
+                        _context.UserRoles.Any(y => y.UserId == x.Id && y.RoleId == teacherRole.Id)), "Id", "Email");
+            
             return View();
         }
 
@@ -97,16 +151,18 @@ namespace TOS.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("TopicId,Name,DescriptionShort,DescriptionLong,Visible,CreatorId,SupervisorId,AssignedId,GroupId")] Topic topic)
         {
+            topic.CreatorId = _context.Users.First(x => User.Identity != null && x.Email != null && x.Email.Equals(User.Identity.Name)).Id;
+
             if (ModelState.IsValid)
             {
                 _context.Add(topic);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AssignedId"] = new SelectList(_context.Users, "Id", "Id", topic.AssignedId);
-            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", topic.CreatorId);
-            ViewData["GroupId"] = new SelectList(_context.Groups.Where(x=>x.Selectable), "GroupId", "GroupId", topic.GroupId);
-            ViewData["SupervisorId"] = new SelectList(_context.Users, "Id", "Id", topic.SupervisorId);
+            
+            ViewData["Students"] = new SelectList(_context.Users, "Id", "Id", topic.AssignedId);
+            ViewData["Groups"] = new SelectList(_context.Groups.Where(x=>x.Selectable), "GroupId", "GroupId", topic.GroupId);
+            ViewData["Supervisors"] = new SelectList(_context.Users, "Id", "Id", topic.SupervisorId);
             return View(topic);
         }
 
@@ -213,6 +269,33 @@ namespace TOS.Controllers
         private bool TopicExists(int id)
         {
           return (_context.Topics?.Any(e => e.TopicId == id)).GetValueOrDefault();
+        }
+
+        public async Task<JsonResult> Interest(string? topicId)
+        {
+            if (topicId == null) throw new Exception("topicId should be provided");
+            
+            //Current user
+            var user = await _context.Users.FirstAsync(x => User.Identity != null && x.Email != null && x.Email.Equals(User.Identity.Name));
+            
+            //Topic
+            var topic = await _context.Topics.FirstAsync(x => x.TopicId.ToString().Equals(topicId));
+
+            if (await _context.UserInterestedTopics.AnyAsync(x => x.UserId.Equals(user.Id) && x.TopicId.Equals(topic.TopicId)))
+            {
+                _context.UserInterestedTopics.Remove(_context.UserInterestedTopics.First(x => x.UserId.Equals(user.Id) && x.TopicId.Equals(topic.TopicId)));
+            }
+            else
+            {
+                _context.UserInterestedTopics.Add(new UserInterestedTopic()
+                {
+                    User = user,
+                    Topic = topic
+                });
+            }
+            
+            await _context.SaveChangesAsync();
+            return Json(true);
         }
     }
 }
