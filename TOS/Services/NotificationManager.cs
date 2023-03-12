578b6ec388c4b3e3136a3f389fcaee6db9d000d5
martinikf <martinik.filip01@gmail.com>
2023-03-12 23:18:15 +0100

    Notification refactor

--- TOS/Services/NotificationManager.cs
+++ TOS/Services/NotificationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using NuGet.Packaging;
@@ -20,8 +21,7 @@ public class NotificationManager : INotificationManager
     
     public async Task TopicEdit(Topic topic, ApplicationUser author, string callbackUrl)
     {
-        var notification = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "TopicEdit");
-        if (notification is null) throw new Exception("Database is not seeded");
+        var notification = await GetNotification("TopicEdit");
         
         //Prepare users to notify
         var users = new HashSet<ApplicationUser>();
@@ -34,20 +34,13 @@ public class NotificationManager : INotificationManager
         users.Remove(author);
         users.RemoveWhere(u => u.UserSubscribedNotifications.Any(x => x.NotificationId == notification.NotificationId) == false);
 
-        var subjectF = $"{Parameterize(topic, notification.Subject, callbackUrl, null)} - {Parameterize(topic, notification.SubjectEng, callbackUrl, null)}";
-        var bodyF = $"{Parameterize(topic, notification.Text, callbackUrl, null)}\n---\n{Parameterize(topic, notification.TextEng, callbackUrl, null)}";
-        
-        foreach (var user in users)
-        {
-            await _emailSender.SendEmailAsync(user.Email!, subjectF, bodyF);
-        }
+        await SendNotification(users, topic, notification, null, callbackUrl);
     }
 
     public async Task NewComment(Comment comment, string callbackUrl)
     {
         var topic = comment.Topic;
-        var notification = _context.Notifications.FirstOrDefault(x=>x.Name == "CommentNew");
-        if (notification is null) throw new Exception("Database is not seeded");
+        var notification = await GetNotification("CommentNew");
         
         var users = new HashSet<ApplicationUser>();
         if (topic.AssignedStudent != null) users.Add(topic.AssignedStudent);
@@ -63,20 +56,13 @@ public class NotificationManager : INotificationManager
         users.Remove(comment.Author);
         users.RemoveWhere(u=>u.UserSubscribedNotifications.Any(x=>x.NotificationId == notification.NotificationId) == false);
         
-        var subjectF = $"{Parameterize(comment.Topic, notification.Subject, callbackUrl, comment)} - {Parameterize(comment.Topic, notification.SubjectEng, callbackUrl, comment)}";
-        var bodyF = $"{Parameterize(comment.Topic, notification.Text, callbackUrl, comment)}\n---\n{Parameterize(comment.Topic, notification.TextEng, callbackUrl, comment)}";
-        
-        foreach (var user in users)
-        {
-            await _emailSender.SendEmailAsync(user.Email!, subjectF, bodyF);
-        }
+        await SendNotification(users, topic, notification, comment, callbackUrl);
     }
 
     public async Task TopicAssigned(Topic topic, ApplicationUser author, string callbackUrl)
     {
-        var nStudent = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "TopicAssigned-Student");
-        var nOthers = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "TopicAssigned-Others");
-        if (nStudent is null || nOthers is null) throw new Exception("Database is not seeded");
+        var nStudent = await GetNotification("TopicAssigned-Student");
+        var nOthers = await GetNotification("TopicAssigned-Others");
         
         //Prepare users to notify
         var users = new HashSet<ApplicationUser>();
@@ -88,50 +74,26 @@ public class NotificationManager : INotificationManager
         
         users.Remove(author);
         users.RemoveWhere(u => u.UserSubscribedNotifications.Any(x => x.NotificationId == nOthers.NotificationId || x.NotificationId == nStudent.NotificationId) == false);
-
-        var subjectStudent = $"{Parameterize(topic, nStudent.Subject, callbackUrl, null)} - {Parameterize(topic, nStudent.SubjectEng, callbackUrl, null)}";
-        var bodyStudent = $"{ Parameterize(topic, nStudent.Text, callbackUrl, null)}\n---\n{Parameterize(topic, nStudent.TextEng, callbackUrl, null)}";
         
-        var subjectOthers = $"{Parameterize(topic, nOthers.Subject, callbackUrl, null)} - {Parameterize(topic, nOthers.SubjectEng, callbackUrl, null)}";
-        var bodyOthers = $"{Parameterize(topic, nOthers.Text, callbackUrl, null)}\n---\n{Parameterize(topic, nOthers.TextEng, callbackUrl, null)}";
-        
-        foreach (var user in users)
-        {
-            if (user == topic.AssignedStudent)
-            {
-                await _emailSender.SendEmailAsync(user.Email!, subjectStudent, bodyStudent);
-            }
-            else
-            {
-                await _emailSender.SendEmailAsync(user.Email!, subjectOthers, bodyOthers);
-            }
-        }
+        await SendNotification(users.Where(x => x.Id == topic.AssignedId).ToList(), topic, nStudent, null, callbackUrl);
+        await SendNotification(users.Where(x => x.Id != topic.AssignedId).ToList(), topic, nOthers, null, callbackUrl);
     }
 
     public async Task TopicAdopted(Topic topic, string callbackUrl)
     {
-        var notification = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "TopicAdopted");
-        if (notification is null) throw new Exception("Database is not seeded");
+        var notification = await GetNotification("TopicAdopted");
         
         //Prepare users to notify
-        var users = new HashSet<ApplicationUser>();
-        
-        users.Add(topic.Creator);
+        var users = new HashSet<ApplicationUser> {topic.Creator};
+
         if (topic.AssignedStudent != null) users.Add(topic.AssignedStudent);
-        
-        var subjectF = $"{Parameterize(topic, notification.Subject, callbackUrl, null)} - { Parameterize(topic, notification.SubjectEng, callbackUrl, null)}";
-        var bodyF = $"{Parameterize(topic, notification.Text, callbackUrl, null)}\n---\n{Parameterize(topic, notification.TextEng, callbackUrl, null)}";
-        
-        foreach (var user in users)
-        {
-            await _emailSender.SendEmailAsync(user.Email!, subjectF, bodyF);
-        }
+
+        await SendNotification(users, topic, notification, null, callbackUrl);
     }
 
     public async Task NewInterest(Topic topic, ApplicationUser user, string callbackUrl)
     {
-        var notification = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "NewInterest");
-        if (notification is null) throw new Exception("Database is not seeded");
+        var notification = await GetNotification("NewInterest");
 
         var users = topic.UserInterestedTopics.Select(x => x.User).ToList();
         if(topic.Supervisor != null)
@@ -142,25 +104,31 @@ public class NotificationManager : INotificationManager
             x.UserSubscribedNotifications.Any(y =>
                 y.UserId == x.Id && y.NotificationId == notification.NotificationId) == false).ToListAsync();
         usersToRemove.ForEach(x=> users.Remove(x));
-        
-        var subject = $"{Parameterize(topic, notification.Subject, callbackUrl, null)} - {Parameterize(topic, notification.SubjectEng, callbackUrl, null)}";
-        var body = $"{Parameterize(topic, notification.Text, callbackUrl, null)}\n---\n{Parameterize(topic, notification.TextEng, callbackUrl, null)}";
-        
-        foreach (var u in users)
-        {
-            await _emailSender.SendEmailAsync(u.Email!, subject, body);
-        }
+
+        await SendNotification(users, topic, notification, null, callbackUrl);
     }
 
     public async Task NewExternalUser(ApplicationUser user)
     {
-        var notification = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "NewExternalUser");
-        if (notification is null) throw new Exception("Database is not seeded");
+        var notification = await GetNotification("NewExternalUser");
         
         var users = await _context.UserSubscribedNotifications.Where(x=>x.NotificationId == notification.NotificationId).Select(x=>x.User).ToListAsync();
+
+        await SendNotification(users, null, notification, null, null);
+    }
+
+    private async Task<Notification> GetNotification(string name)
+    {
+        var notification = await _context.Notifications.FirstOrDefaultAsync(x=>x.Name == "name");
+        if (notification is null) throw new Exception($"Database is not seeded. Notification ${name} is missing.");
         
-        var subject = $"{Parameterize(null, notification.Subject, null, null)} - {Parameterize(null, notification.SubjectEng, null, null)}";
-        var body = $"{Parameterize(null, notification.Text, null, null)}\n---\n{Parameterize(null, notification.TextEng, null, null)}";
+        return notification;
+    }
+
+    private async Task SendNotification(IEnumerable<ApplicationUser> users, Topic? topic, Notification notification, Comment? comment, string? callbackUrl)
+    {
+        var subject = $"{Parameterize(topic, notification.Subject, callbackUrl, comment)} - {Parameterize(topic, notification.SubjectEng, callbackUrl, comment)}";
+        var body = $"{Parameterize(topic, notification.Text, callbackUrl, comment)}\n---\n{Parameterize(topic, notification.TextEng, callbackUrl, comment)}";
         
         foreach (var u in users)
         {
@@ -168,25 +136,21 @@ public class NotificationManager : INotificationManager
         }
     }
     
-    private static string Parameterize(Topic topic, string text, string callbackUrl, Comment? comment)
+    private static string Parameterize(Topic? topic, string text, string? callbackUrl, Comment? comment)
     {
-        var topicTypeCz = string.Empty;
-        switch (topic.Type)
-        {
-            case TopicType.Thesis:
-                topicTypeCz = "Závěrečná práce";
-                break;
-            case TopicType.Homework:
-                topicTypeCz = "Domácí úkol";
-                break;
-            case TopicType.Project:
-                topicTypeCz = "Projekt";
-                break;
-        }
+        var sb = new StringBuilder(text);
         
-        var returnString =
-            text.Replace("[URL]", callbackUrl)
-                .Replace("[TOPIC_NAME]", topic.Name)
+        if (topic != null)
+        {
+            var topicTypeCz = topic.Type switch
+            {
+                TopicType.Thesis => "Závěrečná práce",
+                TopicType.Homework => "Úkol",
+                TopicType.Project => "Projekt",
+                _ => string.Empty
+            };
+            
+            sb.Replace("[TOPIC_NAME]", topic.Name)
                 .Replace("[TOPIC_NAME_ENG]", topic.NameEng)
                 .Replace("[TOPIC_SHORT_DESCRIPTION]", topic.DescriptionShort)
                 .Replace("[TOPIC_SHORT_DESCRIPTION_ENG]", topic.DescriptionShortEng)
@@ -200,17 +164,21 @@ public class NotificationManager : INotificationManager
                 .Replace("[TOPIC_ASSIGNED_EMAIL]", topic.AssignedStudent?.Email ?? "-")
                 .Replace("[TOPIC_TYPE]", topicTypeCz)
                 .Replace("[TOPIC_TYPE_ENG]", topic.Type.ToString());
+        }
+
+        if (callbackUrl != null)
+            sb.Replace("[URL]", callbackUrl);
+                
         
         if (comment != null)
         {
-            returnString = returnString
-                .Replace("[COMMENT_AUTHOR]", comment.Author.GetDisplayName())
+            sb.Replace("[COMMENT_AUTHOR]", comment.Author.GetDisplayName())
                 .Replace("[COMMENT_AUTHOR_EMAIL]", comment.Author.Email)
                 .Replace("[COMMENT_TEXT]", comment.Text)
                 .Replace("[COMMENT_DATE]", comment.CreatedAt.ToString(CultureInfo.GetCultureInfo("cs-CZ")));
         }
 
-        return returnString;
+        return sb.ToString();
     }
     
 }
