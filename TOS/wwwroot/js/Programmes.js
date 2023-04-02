8744435a3b882d3c5b982d1451f3d8da6b5c1a4a
martinikf <martinik.filip01@gmail.com>
2023-04-02 21:46:43 +0200

    JS

---
+++ TOS/wwwroot/js/Programmes.js
@@ -0,0 +1,33 @@
+﻿window.onload = function () {
+    RefreshProgrammes(true);
+}
+
+function RefreshProgrammes(skip = false){
+    var programmes = document.getElementsByClassName("Programme");
+    //Hide all programmes and unselect all checkboxes
+    for (var i = 0; i < programmes.length; i++) {
+        programmes[i].style.display = "none";
+        if (!skip)
+            programmes[i].getElementsByTagName("input")[0].checked = false;
+    }
+
+    //Get selected group selected option name
+    const selectElement = document.querySelector('#GroupId');
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+    const groupName = selectedOption.text;
+
+    var bachelorProgrammes = document.getElementsByClassName("Bachelor");
+    var masterProgrammes = document.getElementsByClassName("Master");
+    var toShow = [];
+
+    if (groupName === "Bachelor" || groupName === "Bakalářská"){
+        toShow = bachelorProgrammes;
+    }
+    else if (groupName === "Master" || groupName === "Diplomová"){
+        toShow = masterProgrammes;
+    }
+
+    for (i = 0; i < toShow.length; i++) {
+        toShow[i].style.display = "block";
+    }
+}
